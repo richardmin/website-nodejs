0c219834cfb29831aa78205f32dd0762bd078ad5
Richard Min <richardmin97@gmail.com>
2016-12-25 00:31:28 -0800

    Hot reloading support w/ webpack + tsc added

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,39 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+// Import React and React DOM
+import * as React from 'react';
+import { render } from 'react-dom';
+// Import the Hot Module Reloading App Container – more on why we use 'require' below
+const { AppContainer } = require('react-hot-loader');
 
-import { Hello } from "./components/Hello";
-import { TopBar } from "./components/TopBar";
+// Import our App container (which we will create in a second)
+import App from 'containers/App';
 
-ReactDOM.render(
-    
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example"),
+// Tell Typescript that there is a global variable called module - see below
+declare var module: { hot: any };
 
-);
+// Get our root element
+const rootEl = document.getElementById('app');
 
-ReactDOM.render(
-
-    <TopBar compiler="TypeScript" framework="React" />,
-    document.getElementById("topbar")
-    
+// And render our App into it, inside the HMR App Container which handles the reloading
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  rootEl
 )
+
+// Handle hot reloading actions from Webpack
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    // If we receive a HMR action for our App container, then reload it
+    // using require (we can't do this dynamically with import)
+    const NextApp = require('./containers/App').default;
+
+    // And render it into our root element again
+    render(
+      <AppContainer>
+         <NextApp />
+      </AppContainer>,
+      rootEl
+    )
+  })
+}
