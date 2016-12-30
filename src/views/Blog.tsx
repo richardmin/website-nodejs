78c85b6a1e0d29f9b192a136219783ab768e819f
Richard Min <richardmin97@gmail.com>
2016-12-30 02:42:24 -0800

    Minor Route Addition
    * Added bases for Blog, Projects

---
+++ src/views/Blog.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class Blog extends React.Component<any, any> {
+    render() {
+        return <div>This is Blog</div>;
+    }
+}
---
+++ src/views/Projects.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class Projects extends React.Component<any, any> {
+    render() {
+        return <div>This is Projects</div>;
+    }
+}
