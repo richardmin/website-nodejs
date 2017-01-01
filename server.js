f1acf08295b8521a60778e613531137bdff0051d
Richard Min <richardmin97@gmail.com>
2017-01-01 03:05:55 -0800

    Boilerplate Prod Code

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
+require('dotenv').config();
 require('typescript-require');
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./config/webpack.config');
-
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
-
-  console.log('Listening at http://localhost:3000/');
+var mysql = require('mysql');
+var blog_connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'blog'
 });
+
+
+if(process.env.NODE_ENV === 'development') {
+  new WebpackDevServer(webpack(config), {
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('Listening at http://localhost:3000/');
+  });
+}
+else {
+
+}
