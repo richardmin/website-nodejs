d935581441cfeac7e6f418e19229b542f59ec875
Richard Min <richardmin97@gmail.com>
2017-01-01 23:41:02 -0800

    Initial Blog COmponenets added
    * strip_characters to take a substring of the post. Perhaps this is more appropriate to be in the database/what's passing in the strings

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
 
+export interface BlogPostProps { name: string, post: string, date: Date, post_title: string, char_count: number }
 
+export class BlogPost extends React.Component<BlogPostProps, any> {
+    strip_characters(post: string, char_count: number) {
+        if(post.length < char_count) return post;
+        return post.substring(0, char_count - 3) + "...";
 
-export class BlogPost extends React.Component<any, any> {
+    }
 
+    render() {
+        return <div>
+            <h2> {this.props.post_title} </h2>
+            Posted by {this.props.name} posted {this.strip_characters(this.props.post, this.props.char_count)} at {this.props.date.toLocaleTimeString()}
+        </div>;
+    }
 }
