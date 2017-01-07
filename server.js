581f939ce6bbe89c735987dfa95cb9ca2a05652b
Richard Min <richardmin97@gmail.com>
2017-01-06 18:30:40 -0800

    Server minor refactor

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 'use strict';
 require('dotenv').config();
-
 require('typescript-require');
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./config/webpack.config');
-
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var compiler = webpack(config);
 var express = require('express');
 let app = express();
 var path = require('path');
@@ -24,15 +16,14 @@ function databaseConnection(sqlString, values, callback) {
 
     connection.connect(function(err) {
         if (err !== null) {
-            console.log("[MYSQL] Error connecting to mysql:" + err+'\n');
+            console.log("[MYSQL] Error connecting to mysql:" + err);
         }
     });
 
     connection.query(sqlString, values, function(err) {
         connection.end();
-
-        if (err) {
-            throw err;
+        if (err !== null) {
+            console.log("[MYSQL] Error with mysql query:" + err);
         }
         callback.apply(this, arguments);
     });
@@ -40,26 +31,36 @@ function databaseConnection(sqlString, values, callback) {
 
 
 if(process.env.NODE_ENV === "development") {
+
+    var webpack = require('webpack');
+    var config = require('./config/webpack.config');
+    var compiler = webpack(config);
+    var WebpackDevServer = require('webpack-dev-server');
+
+    var webpackDevMiddleware = require('webpack-dev-middleware');
+    var webpackHotMiddleware = require('webpack-hot-middleware');
+
     app.use(webpackDevMiddleware(compiler, {
         publicPath: config.output.publicPath,
         stats: {colors: true},
         hot: true,
         historyApifallBack: true
-    }))
+    }));
 
     app.use(webpackHotMiddleware(compiler, {
         log: console.log
-    }))
+    }));
 }
 
 app.get('/api/blog/:postId', function(req, res) {
     let query = "SELECT * FROM posts where ID = " + req.params.postId;
     databaseConnection(query, null, function(err, rows) {
         if(err) {
-            console.log("[MYSQL]: MYSQL query failed. Query: " + query + " Result: " + err);
+            res.status(503).json({ error: err.code });
         }
-        res.locals.rows = rows;
-        res.send(rows);
+        else {
+            res.json({ data: rows });
+        }
     });
 });
 
@@ -67,10 +68,11 @@ app.get('/api/blog', function(req, res) {
     let query = "SELECT * FROM posts";
     databaseConnection(query, null, function(err, rows) {
         if(err) {
-            console.log("[MYSQL]: MYSQL query failed. Query: " + query + " Result: " + err);
+            res.status(503).json({ error: err.code });
+        } 
+        else {
+            res.json({ data: rows });
         }
-        res.locals.rows = rows;
-        res.send(rows);
     });
 });
 
@@ -80,5 +82,5 @@ app.get("*", function(req, res) {
 
 app.listen(3000, function(err, res) {
   if (err) return console.log(err);
-  console.log ('listening on 3000')
+  console.log('listening on 3000');
 });
