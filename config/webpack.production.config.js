18b9aa9a248975c7b343d29ac36328a60a925e30
Richard Min <richardmin97@gmail.com>
2017-01-11 20:17:12 -0800

    set node_env for webpack

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -27,6 +27,11 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.DefinePlugin({
+        'process.env': {
+            NODE_ENV: JSON.stringify('production')
+        }
+    }),
     new webpack.optimize.CommonsChunkPlugin('common.js'),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
