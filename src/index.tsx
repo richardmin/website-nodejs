2632d951b07faea3641684de959e936bf03ac3f2
Richard Min <richardmin97@gmail.com>
2017-03-29 19:22:33 -0700

    Hello World now displaying.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import { AppContainer } from 'react-hot-loader';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from 'App';
 
-const rootEl = document.getElementById('root');
-const render = Component =>
-  ReactDOM.render(
-    <AppContainer>
-      <Component />
-    </AppContainer>,
-    rootEl
-  );
 
-render(App);
-if (module.hot) module.hot.accept('App', () => render(App));
+ReactDOM.render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.getElementById('app')
+);
+
+// Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('App', () => {
+    const NextApp = require('App').default;
+    ReactDOM.render(
+      <AppContainer>
+        <NextApp/>
+      </AppContainer>,
+      document.getElementById('app')
+    );
+  });
+}
